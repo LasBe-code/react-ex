305541f0e701e6f0a07e55f1c384d2644d46ddf9
LAPTOP-RUPAEUDS\nuri <jsw0339@nuriflex.co.kr>
2022-09-14 17:50:24 +0900

    Route, param 학습

--- src/ex/components/Header.js
+++ src/ex/components/Header.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const StyledHeaderDiv = styled.div`
   border: 1px solid black;
-  height: 300px;
+  height: 100px;
+  background-color: ${props => props.backgroundColor};
 `;
 
 const Header = () => {
   return (
-    <StyledHeaderDiv>
-      <div>
-        <ul>
-          <li>메뉴 1</li>
-          <li>메뉴 2</li>
-        </ul>
-      </div>
-    </StyledHeaderDiv>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/login/1" className="nav-link">
+              Login
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+    // <StyledHeaderDiv backgroundColor="white">
+    //   <div>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">홈</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/login/10">로그인</Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </StyledHeaderDiv>
   );
 };
 
